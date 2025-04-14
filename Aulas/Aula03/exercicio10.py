87ceba454da1fa3933d62aa9b7c0806ba1486492
Manoel Olímpio <olimpiommelo@gmail.com>
2025-04-14 13:42:45 -0300

    Add files via upload

---
+++ Aulas/Aula03/exercicio10.py
@@ -0,0 +1,35 @@
+mes=int(input(f"Me diga um numero de 1 a 12!\n"))
+if mes>=1 and mes<=12:
+  if mes==1:
+    print(f"Janeiro")
+elif mes==2:
+    print(f"Fevereiro")
+elif mes==3:
+    print(f"Março")
+elif mes==4:
+     print(f"Abril")
+elif mes==5:
+    print(f"Maio")
+elif mes==6:
+    print(f"Junho")
+elif mes==7:
+    print(f"Julho")
+elif mes==8:
+    print(f"Agosto")
+elif mes==9:
+    print(f"Setembro")
+elif mes==10:
+    print(f"Outubro")
+elif mes==11:
+    print(f"Novembro")
+elif mes==12:
+    print(f"Dezembro")
+else:
+    print(f"Mês invalido!")
+##mes=int(input(f"Me diga um numero de 1 a 12!\n"))
+##match mes:
+##case 1:
+##print("Janeiro")
+##case _:
+##print("Numero Invalido")
+
